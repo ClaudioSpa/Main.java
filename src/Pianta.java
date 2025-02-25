7ed6516b7c71334752662d3ea841312dc8932a44
ClaudioSpa <claudiospagnolo06@gmail.com>
2025-02-25 16:54:13 +0100

    Aggiunta file json

--- src/Pianta.java
+++ src/Pianta.java
@@ -1,3 +1,7 @@
 class Pianta{
-    
+    private int etaAdulto;                      //eta' in cui la pianta diventa adulta
+    private int etaMorte;                       //eta' in cui la pianta muore
+    private int eta;                            //eta' della pianta
+    private String nome;                        //Nome pianta (melo, pero, pesca, ...)
+    private int codice;                         //Codice pianta
 }
