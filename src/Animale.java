97b9e1e02593fd0af66fad23a6ee001cadf4db80
ClaudioSpa <claudiospagnolo06@gmail.com>
2025-02-25 18:15:21 +0100

    Getter e setter

--- src/Animale.java
+++ src/Animale.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 class Animale{
     private String nomeSp;                                  //Nome specie (lupo, gatto, rinoceronte, ...)
+    private int codice;                                     //Codice animale
     private char sesso;                                     //Sesso animale, m o f
     private int etaMorte;                                   //Eta' in cui l'animale muore
     private int etaAdulto;                                  //Eta' in cui l'animale diventa adulto
@@ -9,4 +10,105 @@ class Animale{
     private int eta;                                        //Eta' dell'animale
     ArrayList<String> pianteNec = new ArrayList<>();        //Piante necessarie all'animale per sopravvivere
     private int numeroCuc;                                  //Numero di cuccioli dell'animale
+    private int ciboAnnuo;                                  //Cibo annuo necessario all'animale per sopravvivere.
+
+    //GETTERS e SETTERS
+    public String getNomeSp() {
+        return nomeSp;
+    }
+
+    public void setNomeSp(String nomeSp) {
+        this.nomeSp = nomeSp;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public void setCodice(int codice) {
+        this.codice = codice;
+    }
+
+    public char getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(char sesso) {
+        this.sesso = sesso;
+    }
+
+    public int getEtaMorte() {
+        return etaMorte;
+    }
+
+    public void setEtaMorte(int etaMorte) {
+        this.etaMorte = etaMorte;
+    }
+
+    public int getEtaAdulto() {
+        return etaAdulto;
+    }
+
+    public void setEtaAdulto(int etaAdulto) {
+        this.etaAdulto = etaAdulto;
+    }
+
+    public int getNumeroCucMax() {
+        return numeroCucMax;
+    }
+
+    public void setNumeroCucMax(int numeroCucMax) {
+        this.numeroCucMax = numeroCucMax;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public ArrayList<String> getPianteNec() {
+        return pianteNec;
+    }
+
+    public void setPianteNec(ArrayList<String> pianteNec) {
+        this.pianteNec = pianteNec;
+    }
+
+    public int getNumeroCuc() {
+        return numeroCuc;
+    }
+
+    public void setNumeroCuc(int numeroCuc) {
+        this.numeroCuc = numeroCuc;
+    }
+
+    public int getCiboAnnuo() {
+        return ciboAnnuo;
+    }
+
+    public void setCiboAnnuo(int ciboAnnuo) {
+        this.ciboAnnuo = ciboAnnuo;
+    }
+
+    //COSTRUTTORE di tutti i campi
+    public Animale(String nomeSp, int codice, char sesso, int etaMorte, int etaAdulto, int numeroCucMax, int eta,
+            ArrayList<String> pianteNec, int numeroCuc, int ciboAnnuo) {
+        this.nomeSp = nomeSp;
+        this.codice = codice;
+        this.sesso = sesso;
+        this.etaMorte = etaMorte;
+        this.etaAdulto = etaAdulto;
+        this.numeroCucMax = numeroCucMax;
+        this.eta = eta;
+        this.pianteNec = pianteNec;
+        this.numeroCuc = numeroCuc;
+        this.ciboAnnuo = ciboAnnuo;
+    }
+
+    
+    
+    
 }
--- src/Pianta.java
+++ src/Pianta.java
@@ -1,7 +1,61 @@
 class Pianta{
-    private int etaAdulto;                      //eta' in cui la pianta diventa adulta
     private int etaMorte;                       //eta' in cui la pianta muore
     private int eta;                            //eta' della pianta
     private String nome;                        //Nome pianta (melo, pero, pesca, ...)
     private int codice;                         //Codice pianta
+    private int riproduzione;                   //Riproduzione annua della pianta
+
+    //GETTERS e SETTERS
+    public int getEtaMorte() {
+        return etaMorte;
+    }
+
+    public void setEtaMorte(int etaMorte) {
+        this.etaMorte = etaMorte;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public void setCodice(int codice) {
+        this.codice = codice;
+    }
+
+    public int getRiproduzione() {
+        return riproduzione;
+    }
+
+    public void setRiproduzione(int riproduzione) {
+        this.riproduzione = riproduzione;
+    }
+
+    //COSTRUTTORE di tutti gli elementi
+    public Pianta(int etaMorte, int eta, String nome, int codice, int riproduzione) {
+        this.etaMorte = etaMorte;
+        this.eta = eta;
+        this.nome = nome;
+        this.codice = codice;
+        this.riproduzione = riproduzione;
+    }
+
+    
+
+    
 }
